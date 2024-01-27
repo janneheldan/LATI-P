a2ca02e29c2d1b20b29b822274b1e145bf31c162
janne <arvoheldan@gmail.com>
2024-01-27 03:54:42 +0200

    frontend

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from model import predict_passangers
 import pandas as pd
 import datetime
 from holidays import CountryHoliday 
+import requests
 
 app = Flask(__name__)
 scheduler = APScheduler()
@@ -13,7 +14,19 @@ scheduler.start()
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    predictions = []
+    with open('weather_forecast.csv', 'r') as f:
+        next(f)
+        data = f.readlines()
+        for i, line in enumerate(data):
+            line = line.strip().split(',')
+            holi = is_holiday(int(line[2]), int(line[3]), int(line[4]), 'FI') 
+            day_of_week = datetime.datetime(int(line[4]), int(line[3]), int(line[2])).weekday()
+            predictions.append({'x': i + 1, 'y': predict_passangers(float(line[0]), float(line[1])/100, line[2], line[3], day_of_week, holi)})
+
+    print(predictions)
+    return render_template('index.html', predictions=predictions)
+
 
 @app.route('/predict', methods=['GET'])
 def predict_get():
@@ -39,7 +52,6 @@ def predict_post():
 
     prediction = predict_passangers(float(temp), float(rain)/100, int(day), int(month), day_of_week, h)
 
-    print(prediction)
     return render_template('predict.html', prediction=prediction)
 
 def is_holiday(day, month, year, country):
@@ -49,6 +61,17 @@ def is_holiday(day, month, year, country):
     else:
         return 0
 
+def weather_forecast():
+    response = requests.get('https://api.open-meteo.com/v1/forecast?latitude=60.1695&longitude=24.9354&hourly=temperature_2m,rain')
+    data = response.json()
+    df = pd.DataFrame(data['hourly'])
+    df['time'] = pd.to_datetime(df['time'])
+    df.set_index('time', inplace=True)
+    df_daily = df.resample('D').agg({'temperature_2m': 'mean', 'rain': 'sum'})
+    df_daily['day'] = df_daily.index.day
+    df_daily['month'] = df_daily.index.month
+    df_daily['year'] = df_daily.index.year
+    df_daily.to_csv('weather_forecast.csv', index=False)
 
 def get_data():
     print("Getting data...")
